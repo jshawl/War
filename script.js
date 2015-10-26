8d02261af3dee715b9254db5e9925a41be6cc34b
Timothy Gregg <timtgregg@gmail.com>
2015-10-26 10:47:23 -0400

    created deck with suits!!

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 
-var createCard = function(value){
+var createCard = function(value, suit){
   var newCard = {
     value : null,
     suit : null,
   };
   newCard.value=value;
+  newCard.suit=suit;
   return newCard;
 };
 
 var createDeck = function(){
   var newDeck=[];
-  console.log(newDeck);
-  for (var i = 2; i <= 14; i++) {
-    var newCard = createCard(i);
-    console.log(newCard);
-    newDeck.push(newCard);
-    console.log(newDeck);
+  for (var i = 0; i < 4; i++) {
+    var suit = null;
+    if ( i===0 ) {
+      suit='Hearts';
+    } else if ( i===1 ) {
+      suit='Diamonds';
+    } else if ( i===2 ) {
+      suit='Spades';
+    } else if ( i===3 ) {
+      suit='Clubs';
+    }
+    for (var j = 2; j <= 14; j++) {
+      var newCard = createCard(j, suit);
+      newDeck.push(newCard);
+    }
   }
   return newDeck;
 };
 
-createDeck();
+var getCardName = function(card){
+  return card.value + ' of ' + card.suit;
+};
+
+var deck = createDeck();
+console.log(deck);
+console.log(getCardName(deck[45]));
